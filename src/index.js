1087a5cf987010eccf918f6be8ded4436a4af86c
Polina Makarenko <polinamakarenko.com@gmail.com>
2022-07-26 19:47:52 +0200

    docs: add result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+   let result = '';
+   num = Math.abs(num); //math.abs() - абсолютное число
+   num = String(num);
+   for (let i = 0; i < num.length; i++) {
+     result = `${num[i]}${result}`
+   };
+   result = Number(result);
+   return result;
+
 }
+
+console.log(module.exports);
